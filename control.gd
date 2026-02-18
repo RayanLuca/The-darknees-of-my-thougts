8ca09de92b2a24d6c81da8e51dc5aae6b587b1a5
Rayan Luca <rayanlucateixeira10@gmail.com>
2026-02-18 15:46:47 -0300

    game bata 01

---
+++ control.gd
@@ -0,0 +1,12 @@
+extends Control
+
+
+
+
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scene/game.tscn")
+
+
+func _on_button_2_pressed() -> void:
+	get_tree().quit()
--- scripts/osso_do_pedrinho.gd
+++ scripts/osso_do_pedrinho.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
-var speed = 60
+var speed = 80
 var direction = 1
  
 func _process(delta: float) -> void:
