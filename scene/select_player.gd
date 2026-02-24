beddc00a10f1ec1210fef9a23f5a3ab6173603ad
Rayan Luca <rayanlucateixeira10@gmail.com>
2026-02-24 20:39:10 -0300

    Atualização

--- scene/select_player.gd
+++ scene/select_player.gd
@@ -5,20 +5,16 @@ var personagem_selecionado: String = ""
 @onready var player_button = $PlayerButton
 @onready var walter_button = $WalterButton
 
-func _ready():
+func _ready() -> void:
 	print("Tela de seleção carregada")
 
-func _on_player_button_toggled(button_pressed: bool) -> void:
-	if button_pressed:
-		walter_button.button_pressed = false
-		personagem_selecionado = "player"
-		print("Player selecionado")
+func _on_player_button_pressed() -> void:
+	personagem_selecionado = "player"
+	print("Player selecionado")
 
-func _on_walter_button_toggled(button_pressed: bool) -> void:
-	if button_pressed:
-		player_button.button_pressed = false
-		personagem_selecionado = "walter"
-		print("Walter selecionado")
+func _on_walter_button_pressed() -> void:
+	personagem_selecionado = "walter"
+	print("Walter selecionado")
 
 func _on_confirmation_button_pressed() -> void:
 	print("Confirmar clicado")
